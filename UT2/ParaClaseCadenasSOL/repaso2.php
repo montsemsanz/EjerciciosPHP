3b39b820b6388d23f392ba36c8fb4e2d1867ea2e
Montse Viyuela <montsemsanz@gmail.com>
2018-11-14 17:18:24 +0100

    Modificado de UT2 - ParaClaseCadenasSOL

--- UT2/ParaClaseCadenasSOL/repaso2.php
+++ UT2/ParaClaseCadenasSOL/repaso2.php
@@ -1,24 +1,20 @@
 ﻿<?php
-
       require_once "includes/cabecera.php";
       require_once "funciones.php";
 
       $nombres = ["Ana", "Luis", "Alberto", "Berta", "Elisa", "Juan", "Isabel"];
-      echo "<h3>Escribimos cada nombre cada nombre en may&uacute;sculas y el total de vocales</h3>\n";
-      foreach ($nombres as $nombre)
-      {
+      echo "<h3>Escribimos cada nombre en may&uacute;sculas y el total de vocales</h3>\n";
+      foreach ($nombres as $nombre) {
           echo "<p>" . strtoupper($nombre) . ":" . contarVocales($nombre) . "</p>\n";
       }
       echo "<h3>Sustituimos ahora en los nombres la 'a' por '*'</h3>\n";
-      foreach ($nombres as $nombre)
-      {
+      foreach ($nombres as $nombre) {
           echo "<p>" . $nombre . " : " . str_replace('a', '*', $nombre) . "</p>\n";
       }
 
       echo "<h3>Escribimos una tabla HTML con cada nombre invertido y guionizado</h3>\n";
       echo "<table>\n";
-      foreach ($nombres as $nombre)
-      {
+      foreach ($nombres as $nombre) {
           $invertido = strrev($nombre);
           $guionizado = guionizar($nombre);
           echo "<tr><td>" . $invertido . " </td><td>" . $guionizado . "</td></tr>\n";
