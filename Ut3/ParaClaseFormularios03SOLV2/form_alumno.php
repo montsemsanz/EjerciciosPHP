3e2a4300ef8c76d00b686f4c2da5b4d9333cce4b
Montse Viyuela <montsemsanz@gmail.com>
2019-01-14 19:50:28 +0100

    Añadido ParaClaseFormularios03SOLV2

---
+++ Ut3/ParaClaseFormularios03SOLV2/form_alumno.php
@@ -0,0 +1,40 @@
+﻿<?php
+    
+    include "cabecera.php";
+?>
+    <form id='form' action="index.php" method="post">
+        <fieldset>
+            <legend>Datos de un alumno</legend>
+             <label>Nombre</label>
+        <input type="text" name="nombre"  /><br />
+        <label>Apellido</label>
+        <input type="text" name="apellido" /><br />
+        <label>Módulo</label> 
+        <select name="modulo" >
+            <option value="bd" >Bases de datos</option>
+            <option value="aweb">Aplicaciones web</option>
+            <option value="seg">Seguridad</option>
+            <option value="so">Sistemas operativos</option>
+        </select><br />
+        <label>Nota</label> 
+        <select name="nota" >
+            <?php
+                for ($i = 1; $i <= 10; $i++)
+                {
+                    echo "<option value='".$i." ' >$i</option>";
+                }
+            ?>
+        </select><br />
+        <label>&nbsp;</label>
+        <input type="submit" name="enviar" value="Enviar" /><br />
+        </fieldset>
+       
+        
+    </form>
+<?php
+    if (isset($resultado))
+    {
+        echo $resultado;
+    }
+    include "pie.php";
+?>
